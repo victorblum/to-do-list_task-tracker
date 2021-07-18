44cc9c4c386ad85735f3ce48541a268f42c8d760
victorblum <viktor.blumov@gmail.com>
2021-07-18 19:18:26 +0300

    update

--- script.js
+++ script.js
@@ -1,66 +1,109 @@
 const sortButton = document.getElementById('sort');
 const enterButton = document.getElementById('enter');
 const deleteButton = document.getElementById('deleteTask');
-const liDeleteButton = document.querySelectorAll('.liButton');
+const mainForm = document.getElementById('taskForm');
+const plusButton = document.querySelector('.plus');
 
 const taskRow = document.getElementById('newTask');
-const ulTaskListElement = document.querySelector('.taskList ul');
 const taskListElement = document.querySelector('.taskList');
-const liElement = document.querySelectorAll('.edit-list')
+const ulTaskListElement = document.querySelector('.taskList ul');
 
-enterButton.addEventListener('click', clickEnterButton);
-deleteButton.addEventListener('click', clickDeleteButton);
-ulTaskListElement.addEventListener('click', clickEditButton);
-// liDeleteButton.addEventListener('click', clickLiDeleteButton);
-// sortButton.addEventListener('click', clickSortButton);
+// Change main button color
+enterButton.addEventListener('mouseover', function() {
+    plusButton.style.background='#AA68FE';
+    enterButton.style.background='#9953F1';
+})
+enterButton.addEventListener('mouseout', function() {
+    plusButton.style.background='#9953F1';
+    enterButton.style.background='#833AE0';
+})
 
-
+// Writing and adding new task
+mainForm.addEventListener('submit', clickEnterButton);
 function clickEnterButton(event) {
+    event.preventDefault();
     if (taskRow.value.trim() === '' || taskRow.value === null) {
         return false;
     } else {
     const newLi = document.createElement('li');
-    newLi.innerText=taskRow.value;
-    newLi.classList.add("edit-list");
+    const liSpan = document.createElement('span');
+    newLi.classList.add("liItem");
+    liSpan.classList.add("spanItem");
+    liSpan.innerText=taskRow.value;
     ulTaskListElement.append(newLi);
+    newLi.append(liSpan);
+    newLi.draggable=true;
+    liSpan.contentEditable=true;
+    liSpan.setAttribute('max', 25);
 
-    const newButton = document.createElement('button')
-    newButton.innerText="+";
+    const newButton = document.createElement('span')
+    newButton.innerHTML="&times;";
     newButton.classList.add("liButton");
     newLi.append(newButton);
-
+    
     taskRow.value='';
     taskListElement.style.display='block';
+    liEditButton();
+    liDeleteButton(newButton);
     }
 }
 
-taskRow.addEventListener("keypress", (event) => {
-    const keyEnter = 13;
-    if (event.which === keyEnter) {
-        clickEnterButton();
-    }
+// Task editing
+function liEditButton() {
+    const textfields = document.querySelectorAll(".spanItem"); 
+    for(i=0; i<textfields.length; i++){
+    textfields[i].addEventListener("keypress", function(e) {
+        if (e.shiftKey || e.which === 13) {
+            e.preventDefault();
+          } else if (this.textContent.length >= this.getAttribute("max")){
+            e.preventDefault();
+            return false;
+        }
+    }, false);
+}}
+
+// Deleting a task
+function liDeleteButton(button) {
+    button.addEventListener("click", (event) => {
+        const liElements = document.querySelectorAll('.liItem');
+        if (liElements.length === 1) {
+            button.parentElement.remove();
+            event.stopPropagation();
+            taskListElement.style.display='none';
+        } else {
+            button.parentElement.remove();
+            event.stopPropagation();
+        }
+    });
+}
+
+// Sorting tasks
+sortButton.addEventListener('click', liSortButton);
+function liSortButton() {
+const liSpanElements = document.querySelectorAll('.liItem span:first-child');
+const liElements = document.querySelectorAll('.liItem');
+
+if (liElements.length >= 2) {
+let list = [];
+for(let i=0; i<liSpanElements.length; i++){
+    list.push(liSpanElements[i]);
+}
+list.sort((a, b) => {
+    const first = a.innerHTML;
+    const second = b.innerHTML;
+    return first < second ? -1 : (first  > second ? 1 : 0);
 });
+for(let i=0; i<list.length; i++){
+    ulTaskListElement.insertBefore(list[i], ulTaskListElement.firstChild);
+    console.log(list[i].innerHTML);
+    }
 
-function clickDeleteButton(event) {
-    event.preventDefault();
-    taskRow.value='';
+sortButton.style.background="url('images/Group\ 90.svg') no-repeat";
+    sortButton.onmouseover = () => {
+        sortButton.style.background="url('images/Group\ 91.svg') no-repeat";
+    }
+    sortButton.onmouseout = () => {
+        sortButton.style.background="url('images/Group\ 90.svg') no-repeat";
+    }
 }
-
-function clickEditButton (event) {
-    const liElem = event.target;
-    liElem.contentEditable = true;
 }
-
-// function clickLiDeleteButton(element) {
-//         element.addEventListener("click", (event) => {
-//             element.parentElement.remove();
-//             event.stopPropagation();
-//         });
-
-// function clickSortButton () {
-
-// }
-
-// function dragAndDrop () {
-
-// }
